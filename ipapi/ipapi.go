a3bec161e38f4dd4020dd204510b976fa8992f82
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-09-13 16:38:42 +0700

    Remove all symbol from org field

--- ipapi/ipapi.go
+++ ipapi/ipapi.go
@@ -2,7 +2,12 @@ package ipapi
 
 import (
 	"encoding/json"
+	"regexp"
 	"strings"
+)
+
+var (
+	symbolRegex = regexp.MustCompile("[^a-zA-Z0-9 ]")
 )
 
 func Parse(str string) Ipapi {
@@ -19,6 +24,7 @@ func Parse(str string) Ipapi {
 			if ipapi.CountryCode == country.Code {
 				ipapi.Region = country.Region
 				ipapi.CountryName = country.Name
+				ipapi.Org = symbolRegex.ReplaceAllString(ipapi.Org, "")
 				break
 			}
 		}
