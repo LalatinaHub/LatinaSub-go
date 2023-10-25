19cd61f1d865d93cd28f16cd311b50782a59b18a
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-10-25 09:42:58 +0700

    Read args

--- cmd/latinasub/main.go
+++ cmd/latinasub/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	latinasub "github.com/LalatinaHub/LatinaSub-go"
 )
 
+var (
+	saveToDB = false
+)
+
 func main() {
 	// Start the main func
-	latinasub.Start([]string{}, false)
+	for _, arg := range os.Args {
+		switch arg {
+		case "save_to_db":
+			saveToDB = true
+		}
+	}
+
+	latinasub.Start([]string{}, saveToDB)
 
 	fmt.Printf("\n==============================\n")
 	fmt.Println("Software will exit in 10 second !")
